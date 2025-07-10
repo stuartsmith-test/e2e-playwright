7c1d6021f21e08bae2bd35c714a6cfdd81a2e59a
Stuart Smith <stuart.p.smith@gmail.com>
2025-07-10 17:35:51 -0500

    Force headless browser for GitHub Actions

--- playwright-saucedemo/tests/test_login.py
+++ playwright-saucedemo/tests/test_login.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright, expect
 
 def test_login_success():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)  # Change to True to hide browser
+        browser = p.chromium.launch(headless=True)  # Change to True to hide browser
         page = browser.new_page()
         page.goto("https://www.saucedemo.com/")
         page.fill("#user-name", "standard_user")
